eab377160ec9ccb0f1d901c289ffca03f0bd9fb2
Mark Bonnici <mb6792@gmail.com>
2022-02-04 23:41:27 +0100

    build: add version to flare on reporting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Flare;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Flare::determineVersionUsing(function () {
+            return config('app.version');
+        });
     }
 }
